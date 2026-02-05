7f4cd09958c6559bb705d15064276a5ceab64a2c
xxxmetalin <xxxkochoran@gmail.com>
2026-02-05 14:27:29 +0900

    scrool banner

---
+++ js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const fixedCta = document.getElementById('fixedCta');
+    const showTarget = document.querySelector('.cta');      // これが見切れたら表示
+    const hideTarget = document.querySelector('.faq');      // ここで消す
+  
+    window.addEventListener('scroll', function() {
+      const scrollY = window.scrollY;
+      const windowH = window.innerHeight;
+      
+      // .ctaの下端が画面上部に消えたら表示
+      const showPoint = showTarget.offsetTop + showTarget.offsetHeight;
+      const hidePoint = hideTarget.offsetTop - windowH;
+  
+      if (scrollY > showPoint && scrollY < hidePoint) {
+        fixedCta.classList.add('is-show');
+      } else {
+        fixedCta.classList.remove('is-show');
+      }
+    });
+  });
