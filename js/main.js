bc95d77ac3d1772c28464d1f17559905b22e86b3
xxxmetalin <xxxkochoran@gmail.com>
2026-02-05 19:10:44 +0900

    cta banner add

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const fixedCta = document.getElementById('fixedCta');
-    const showTarget = document.querySelector('.cta');      // これが見切れたら表示
-    const hideTarget = document.querySelector('.faq');      // ここで消す
+
+// 追従CTAの表示制御
+// document.addEventListener('DOMContentLoaded', function() {
+//     const fixedCta = document.getElementById('fixedCta');
+//     const showTarget = document.querySelector('.cta');      // これが見切れたら表示
+//     const hideTarget = document.querySelector('.faq');      // ここで消す
   
-    window.addEventListener('scroll', function() {
-      const scrollY = window.scrollY;
-      const windowH = window.innerHeight;
+//     window.addEventListener('scroll', function() {
+//       const scrollY = window.scrollY;
+//       const windowH = window.innerHeight;
       
-      // .ctaの下端が画面上部に消えたら表示
-      const showPoint = showTarget.offsetTop + showTarget.offsetHeight;
-      const hidePoint = hideTarget.offsetTop - windowH;
+//       // .ctaの下端が画面上部に消えたら表示
+//       const showPoint = showTarget.offsetTop + showTarget.offsetHeight;
+//       const hidePoint = hideTarget.offsetTop - windowH;
   
-      if (scrollY > showPoint && scrollY < hidePoint) {
-        fixedCta.classList.add('is-show');
-      } else {
-        fixedCta.classList.remove('is-show');
-      }
-    });
-  });
+//       if (scrollY > showPoint && scrollY < hidePoint) {
+//         fixedCta.classList.add('is-show');
+//       } else {
+//         fixedCta.classList.remove('is-show');
+//       }
+//     });
+//   });
